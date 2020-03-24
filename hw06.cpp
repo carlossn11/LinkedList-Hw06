0d88b01800bf38f1fe28c720c2097ef9717456fb
carlossn11 <carlossn9511@gmail.com>
2020-03-24 13:57:26 -0700

    Back to normal

--- hw06.cpp
+++ hw06.cpp
@@ -1,15 +1,6 @@
-#include <mach/host_info.h>
 #include "hw06.h"
 
-int main() {
-    host_can_has_debugger_info_data_tcs
-    canHasDebuggerInfoDataTcsc
-    scs
-    w
-
-    ww
-    www
-    www
-
+int main()
+{
 
 }
