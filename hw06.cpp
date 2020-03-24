98639c2455b50c284729150eefdb87cf76ebb284
carlossn11 <carlossn9511@gmail.com>
2020-03-24 13:54:53 -0700

    test

--- hw06.cpp
+++ hw06.cpp
@@ -1,5 +1,15 @@
+#include <mach/host_info.h>
 #include "hw06.h"
 
 int main() {
+    host_can_has_debugger_info_data_tcs
+    canHasDebuggerInfoDataTcsc
+    scs
+    w
+
+    ww
+    www
+    www
+
 
 }
